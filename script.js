dd97514aa05442ab5f6dad1ba7e19dea15d73934
fakeArmy <jjunny0109@gmail.com>
2023-08-25 12:02:35 +0900

    feat : Call API
    - Make API Call feature
    - Have to add conditional statement that decide call second API

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // API Object for the Korean Dictionary API
 const API = {
-  KEY: '',
-  URL: 'https://stdict.korean.go.kr/api/search.do',
+  KEY: 'EF487D8CEBAEF6EAAC70A43617AA1EEC',
+  URL: 'https://stdict.korean.go.kr/api/view.do?certkey_no=5868&type_search=view',
+ //예시 : 물 = https://stdict.korean.go.kr/api/view.do?certkey_no=5868&type_search=view&key=EF487D8CEBAEF6EAAC70A43617AA1EEC&req_type=json&method=WORD_INFO&q=물
 }
+
 
 // Search Bar Features
 // Get DOM Elements
@@ -25,8 +27,19 @@ function searchWord() {
     alert('Please enter a word');
   } else {
     alert(word);
-
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const Api_Url = `${proxy}${API.URL}&key=${API.KEY}&req_type=json&method=WORD_INFO&q=${word}0`;
     // 단어를 검색하는 함수를 만들어야 함
+    fetch(Api_Url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("에러 발생!");
+      })
   }
 }
 
